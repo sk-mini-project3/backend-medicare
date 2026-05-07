ecc2b13aafd956241031d28005405950f6cff702
qudwnals <cwjk1008@naver.com>
2026-05-07 16:17:57 +0900

    feat: 초기 레포 구성 및 파트 작업 시작

---
+++ src/test/java/com/emr/medicare/MedicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emr.medicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
